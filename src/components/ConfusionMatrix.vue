6e57a3d631a6db1fef14656afbfdb6921584c557
yingjieweb <1184061078@qq.com>
2020-11-23 22:30:32 +0800

    add adjust dynamic layout button

--- src/components/ConfusionMatrix.vue
+++ src/components/ConfusionMatrix.vue
@@ -2,10 +2,16 @@
   <div class="container">
     <div class="operation">
       <span>混淆矩阵图</span>
-      <el-input-number v-model="num" @change="handleChange" :min="1" :max="4" label="描述文字"></el-input-number>
+      <div class="number">
+        图片总数量：
+        <el-input-number v-model="num" @change="handleChange" :min="1" :max="4" label="描述文字"></el-input-number>
+        <img src="../assets/images/ong.png" alt="" style="width: 21px; margin-top: -2px; margin-right: 3px; cursor: pointer;">
+        <i class="el-icon-menu" style="cursor: pointer;"></i>
+        <i class="el-icon-s-grid" style="cursor: pointer;"></i>
+      </div>
     </div>
     <div class="inner">
-      <img src="../assets/plots/dimensionReduction.jpg" alt="">
+      <img class="double" src="../assets/plots/dimensionReduction.jpg" alt="">
       <img src="../assets/plots/dimensionReduction.jpg" alt="">
     </div>
   </div>
@@ -16,12 +22,17 @@
     name: "ConfusionMatrix",
     data() {
       return {
-        num: 1
+        num: 1,
+        imgArr: []
       }
     },
     methods: {
-      handleChange() {
-
+      handleChange(value) {
+        this.imgArr = [];
+        for (let i = 1; i < value; i++) {
+          this.imgArr.push({src: require(`../assets/plots/confusionMatrix${value}.jpg`)})
+        }
+        console.log(this.imgArr.length)
       }
     }
   }
@@ -32,18 +43,29 @@
     font-size: 24px;
     font-weight: bold;
   }
+
   .operation {
     display: flex;
     justify-content: space-between;
     margin-bottom: 5px;
   }
+
   .inner {
     width: 100%;
     display: flex;
     justify-content: space-around;
   }
 
-  img {
-    width: 48%;
+  .number {
+    width: 390px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
   }
+
+  i {
+    font-size: 28px;
+  }
+
+  img { width: 48%;}
 </style>
