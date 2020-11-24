318ae80f5968ebc4d2046593c08fff164d8c8437
yingjieweb <1184061078@qq.com>
2020-11-24 22:30:30 +0800

    prepare dynamic add layout class

--- src/components/ConfusionMatrix.vue
+++ src/components/ConfusionMatrix.vue
@@ -4,13 +4,13 @@
       <span>混淆矩阵图</span>
       <div class="number">
         图片总数量：
-        <el-input-number v-model="num" @change="handleChange" :min="1" :max="4" label="描述文字"></el-input-number>
-        <img src="../assets/images/ong.png" alt="" style="width: 21px; margin-top: -2px; margin-right: 3px; cursor: pointer;">
-        <i class="el-icon-menu" style="cursor: pointer;"></i>
-        <i class="el-icon-s-grid" style="cursor: pointer;"></i>
+        <el-input-number v-model="num" @change="handleChange" :min="1" label="描述文字"></el-input-number>
+        <img src="../assets/images/ong.png" class="single" @click="setSingle">
+        <i class="el-icon-menu" style="cursor: pointer;" @click="setDouble"></i>
+        <i class="el-icon-s-grid" style="cursor: pointer;" @click="setTriple"></i>
       </div>
     </div>
-    <div class="inner">
+    <div class="inner" :class="{}">
       <img class="double" src="../assets/plots/dimensionReduction.jpg" alt="">
       <img src="../assets/plots/dimensionReduction.jpg" alt="">
     </div>
@@ -28,11 +28,20 @@
     },
     methods: {
       handleChange(value) {
-        this.imgArr = [];
-        for (let i = 1; i < value; i++) {
+        this.imgArr = []
+        for (let i = 1; i <= value; i++) {
           this.imgArr.push({src: require(`../assets/plots/confusionMatrix${value}.jpg`)})
         }
         console.log(this.imgArr.length)
+      },
+      setSingle() {
+        console.log(1111)
+      },
+      setDouble() {
+        console.log(2222)
+      },
+      setTriple() {
+        console.log(3333)
       }
     }
   }
@@ -61,6 +70,13 @@
     display: flex;
     justify-content: space-around;
     align-items: center;
+  }
+
+  .single {
+    width: 21px;
+    margin-top: -2px;
+    margin-right: 3px;
+    cursor: pointer;
   }
 
   i {
